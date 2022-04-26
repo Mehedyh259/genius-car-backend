07c3114f988985d61350e0ee1bccd5dbe94e67ac
Mehedyh259 <mehedyh259@gmail.com>
2022-04-26 12:48:27 +0600

    dd

---
+++ _steps.js
@@ -0,0 +1,15 @@
+/* One time
+* 1. create heroku accout
+2. verify email
+3. install heroku cli
+4. heroku login
+
+
+
+## for each project##
+1. heroku create
+2. git add .  git commit git push
+3. git push heroku main
+
+
+*/
