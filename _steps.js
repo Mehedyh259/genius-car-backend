dfd0445c9f69fb191a968218133d5c939aacda55
Mehedyh259 <mehedyh259@gmail.com>
2022-04-26 19:29:17 +0600

    steps added

--- _steps.js
+++ _steps.js
@@ -10,6 +10,15 @@
 1. heroku create
 2. git add .  git commit git push
 3. git push heroku main
+4. got to heroku dashboard current project and settings>reviel config vers
+5. copy paste config vers from .env file
 
+Update server
+1. make changes
+2. git add .  git commit git push
+3. git push heroku main 
+_______________________
+Connect Server With Client
+1. replace localhost by Heroku server link
 
 */
